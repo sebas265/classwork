51029626fea27c973c81516d285a266ef41619e9
johannesmx <johannes.muljana@ait.nsw.edu.au>
2025-08-04 07:43:09 +0000

    showing products on women page

--- women.php
+++ women.php
@@ -29,7 +29,23 @@ while( $row = $result->fetch_assoc() ) {
         <?php 
         // create cards from products from database
         foreach( $products as $item ){
-            echo "<div class='card'></div>";
+            $image = $item['image'];
+            $id = $item['id'];
+            $name = $item['name'];
+            $price = $item['price'];
+            // card start
+            echo "<div class='card'>";
+            // product image
+            echo "<img src='products/$image'>";
+            // card content
+            echo "<div class='card-content'>";
+            echo "<h4 class='card-title'>$name</h4>";
+            echo "<p class='price'>$price</p>";
+            // card button
+            echo "<a href='#' class='card-button'>Add to cart</a>";
+            echo "</div>";
+            
+            echo "</div>";
         }
         ?>
         <div class="bottom-banner">
