f1c55b06168e17c27e43f954ba983f4c23e8d793
johannesmx <johannes.muljana@ait.nsw.edu.au>
2025-08-11 09:19:28 +0000

    added order page

--- order.php
+++ order.php
@@ -16,6 +16,33 @@ if( $_GET['product_id']) {
     $price = $product['price'];
     $description = $product['description'];
 }
+$submitting = false;
+
+// submitting order to the database
+if( $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $customer_name = $_POST['name'];
+    $email = $_POST['email'];
+    $product_name = $_POST['product_name'];
+    $price = $_POST['price'];
+    $quantity = $_POST['quantity'];
+    $total = $_POST['total'];
+    $date = date('Y-m-d H:i:s');
+
+    $query = "
+    INSERT INTO orders 
+    (customer_name,
+    email,
+    product_name,
+    quantity,
+    price,
+    order_date)
+    VALUES(?,?,?,?,?,?)";
+
+    $statement = $connection -> prepare($query);
+    $statement -> bind_param("sssids",$customer_name,$email,$product_name,$price,$quantity,$date);
+    $statement -> execute();
+    $submitting = true;
+}
 ?>
 
 <!DOCTYPE html>
@@ -27,32 +54,45 @@ if( $_GET['product_id']) {
     <?php include "includes/pageheader.php" ?>
    
     <main class="container">
-        <form id="contact-form" method="post" action="order.php">
+        <form id="order-form" method="post" action="order.php">
             <?php
             if( $submitting == true ) {
-                echo "<p class='alert'>Thank you for contacting us</p>";
+                echo "<p class='alert'>Thank you for your order</p>";
             }
             ?>
             <h3>Your order</h3>
             <label for="name">Your Name</label>
-            <input maxlength="255" type="text" name="name" id="name" placeholder="Jane Smith">
+            <input maxlength="255" required type="text" name="name" id="name" placeholder="Jane Smith">
             <label for="email">Your Email Address</label>
-            <input maxlength="255" type="email" name="email" id="email" placeholder="you@example.com">
+            <input maxlength="255" required type="email" name="email" id="email" placeholder="you@example.com">
             <label for="product_name">Product Name</label>
             <input maxlength="255" type="text" value="<?php echo $name; ?>" name="product_name" id="product_name" placeholder="Hey there">
             <label for="price">Price</label>
             <input type="number" value="<?php echo $price; ?>" readonly id="price" name="price">
             <label for="quantity">Quantity</label>
-            <input type="number" value="1" min="1" name="quantity">
+            <input type="number" value="1" min="1" name="quantity" id="quantity">
             <label for="total">total</label>
-            <input type="number" value="1" min="1" name="quantity">
+            <input type="number" readonly value="<?php echo $price; ?>" min="1" name="total" id="total">
             <div class="buttons">
                 <button type="reset">Cancel</button>
-                <button type="submit">Send</button>
+                <button type="submit">Submit Order</button>
             </div>
         </form>
     </main>
     <?php include "includes/footer.php"; ?>
+    <script>
+        const form = document.querySelector("#order-form")
+        const price = document.querySelector("#price").value
+        const total = document.querySelector("#total")
+        
+        function onChange( event ) {
+           if(event.target.id = "quantity") {
+              total.value = price * event.target.value  
+           }
+           
+        }
+        form.addEventListener("change", onChange )
+    </script>
 </body>
 
 </html>
